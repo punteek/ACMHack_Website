4833b5fa8fb8d030406f951d60ebb40c1128896c
punteek <prateeksingh1@hotmail.com>
2018-03-04 22:35:15 -0800

    start tracking project

---
+++ hack-navbar.js
@@ -0,0 +1,14 @@
+// Attach on click handler for hamburger menu
+// test that click listener is working
+const hamMenu = document.querySelector(".ham-menu");
+const navBar = document.getElementById()
+hamMenu.addEventListener("click", function(){
+	console.log("ham-menu clicked!");
+
+	// add shadow to navbar
+
+});
+
+// we want to toggle the mobile menu when the
+// hamburger menu is clicked
+
