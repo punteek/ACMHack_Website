efb9d27f6fbee5fe34d208b780c3d7a00e338fc9
punteek <prateeksingh1@hotmail.com>
2018-03-12 00:56:28 -0700

    clean comments and spacing

--- hack-navbar.js
+++ hack-navbar.js
@@ -1,20 +1,14 @@
 const hamMenu = document.querySelector(".ham-menu");
-
 const mobileMenu = document.querySelector(".mobile-menu");
+
 // "initialize" display property of mobileMenu
 mobileMenu.style.display = "none";
 
-// Attach on click handler for hamburger menu
-// test that click listener is working
-
+// Make mobile menu appear on click
 hamMenu.addEventListener("click", function(){
-	// test event listener
-	console.log("ham-menu clicked!");
-
 	// toggle mobile menu on click
 	if (mobileMenu.style.display === "none") {
 		mobileMenu.style.display = "block";
-		// mobileMenu.style.opacity = 0;
 		setTimeout(function() {
 			mobileMenu.style.animationPlayState = "pause";
 		}, 1.8);
